83776307fe6e4b5971b895b82ae03253be69ad41
Michael Min <michaelmin2020@gmail.com>
2022-02-24 11:29:06 -0600

    removed commented out code

--- forms-01-starting-setup/src/components/RatingControl.vue
+++ forms-01-starting-setup/src/components/RatingControl.vue
@@ -16,19 +16,8 @@
 export default {
   props: ['modelValue'],
   emits: ['update:modelValue'],
-  // data() {
-  //   return {
-  //     activeOption: this.modelValue,
-  //   };
-  // },
-  // computed: {
-  //   activeOption() {
-  //     return this.modelValue;
-  //   }
-  // },
   methods: {
     activate(option) {
-      //this.activeOption = option;
       this.$emit('update:modelValue', option);
     },
   },
